b2d7763de68cc9be4c698fe0a8359b37f4bb452c
Ryan Tolentino <Tolentino2016@gmail.com>
2020-04-12 11:50:45 -0500

    Added clear function to calc

--- swiftyCalculator/swiftyCalculator/ViewController.swift
+++ swiftyCalculator/swiftyCalculator/ViewController.swift
@@ -9,11 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var numberonTheScreen: Double = 0
+    var previousNumber: Double = 0
+    var operation = 0
+    
+    // Operations tags = = 20 - +/- = 26
+    
+    // AC tag = 27
+    
+    // . tag = 28
+    
     override func viewDidLoad() {
+        output.text = ""
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBOutlet weak var output: UILabel!
+    
+    @IBAction func numberPressed(_ sender: Any) {
+        let tag = (sender as! UIButton).tag
+        
+        output.text = output.text! + String(tag)
+    }
+    
+    @IBAction func operationPressed(_ sender: Any) {
+        
+        let tag = (sender as! UIButton).tag
+        
+        //AC
+        if tag == 27{
+            numberonTheScreen = 0
+            previousNumber = 0
+            operation = 0
+            output.text = ""
+        }
+    }
+    
+    
+
 
 
 }
